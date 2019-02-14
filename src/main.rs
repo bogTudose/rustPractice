6583e2463e1f72a86eed1098b1923798f12bd4c7
bogTudose <root@localhost.localdomain>
2019-02-14 15:34:48 -0500

    added basic error handling and looping

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,30 @@ fn main() {
 
 	println!("the secret number is: {}", secret_number);
 
-	println!("Input your guess");
+	loop{
+
+		println!("Input your guess");
 	
-	let mut guess = String::new();	
+		let mut guess = String::new();	
 	
-	io::stdin().read_line(&mut guess)
-		.expect("Failed to read line");
+		io::stdin().read_line(&mut guess)
+			.expect("Failed to read line");
 
-	let guess: u32 = guess.trim().parse()
-		.expect("Please type a number");
+		let guess: u32 = guess.trim().parse()
+			.expect("Please type a number");
 
-	println!("You guessed: {}",guess);
+		println!("You guessed: {}",guess);
+	
+		println!("Please input your guess");
 
-	match guess.cmp(&secret_number){
-		Ordering::Less => println!("Too small"),
-		Ordering::Greater => println!("Too big"),
-		Ordering::Equal => println!("correct"),
+		match guess.cmp(&secret_number){
+			Ordering::Less => println!("Too small"),
+			Ordering::Greater => println!("Too big"),
+			Ordering::Equal => { 
+				println!("correct");
+				break;
+			},
+		}
 	}
 
 }
